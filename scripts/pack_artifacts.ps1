6ed6cdf2af31ee79fc26a97f32b003d95b145075
George Wu <uiharu@buaa.edu.cn>
2020-05-12 08:33:03 +0200

    Fix artifact packaging

--- scripts/pack_artifacts.ps1
+++ scripts/pack_artifacts.ps1
@@ -13,15 +13,17 @@ $subPaths = @(
     [String]::Format("src/MiriTore.Logging/bin/{0}", $configuration),
     [String]::Format("src/MltdInfoViewer/bin/{0}", $configuration),
     #[String]::Format("src/ScenarioEdit/bin/{0}", $configuration),
-	[String]::Format("src/TDFacial/bin/{0}", $configuration), # remember to include facial_expr.json
+    [String]::Format("src/TDFacial/bin/{0}", $configuration), # remember to include facial_expr.json
     [String]::Format("src/ManifestTools/bin/{0}", $configuration),
     [String]::Empty
 );
 
-foreach ($subPath in $subPaths) {
-    if ([String]::IsNullOrEmpty($subPath)) {
+foreach ($subPath in $subPaths)
+{
+    if ( [String]::IsNullOrEmpty($subPath))
+    {
         continue
-	}
+    }
 
     [String]$fullDirPath = [System.IO.Path]::Combine($basePath, $subPath);
     [String]$xmlFilesPattern = [System.IO.Path]::Combine($fullDirPath, "*.xml")
@@ -29,10 +31,11 @@ foreach ($subPath in $subPaths) {
     Remove-Item $xmlFilesPattern
 
     [String]$allFilesPattern = [System.IO.Path]::Combine($fullDirPath, "*")
-    
-    [ScriptBlock]$scriptBlock = { 7z a $zipName -r $allFilesPattern }
-    Invoke-Command -ScriptBlock $scriptBlock
+
+    & 7z a $zipName -r $allFilesPattern
 }
+
+& 7z a miritore.zip "${env:APPVEYOR_BUILD_FOLDER}\README.html"
 
 Push-AppveyorArtifact $zipName -FileName "miritore-appveyor-v${env:APPVEYOR_BUILD_VERSION}${env:RELEASE_SUFFIX}.zip" -DeploymentName "Versioned"
 Push-AppveyorArtifact $zipName -FileName "miritore-appveyor-latest.zip" -DeploymentName "Latest"

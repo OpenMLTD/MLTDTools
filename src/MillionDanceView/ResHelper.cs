f55d63069f9ffa36e903b5f2087b8963827c0577
George Wu <uiharu@buaa.edu.cn>
2020-05-31 02:04:44 +0200

    [MillionDanceView] Body->Head

--- src/MillionDanceView/ResHelper.cs
+++ src/MillionDanceView/ResHelper.cs
@@ -77,7 +77,7 @@ namespace OpenMLTD.MillionDance.Viewer {
                         throw new ArgumentNullException(nameof(mesh), "One of head meshes is null.");
                     }
 
-                    var m = new MeshWrapper(manager.assetsFileList, mesh, TexturedMaterialExtraProperties.Body);
+                    var m = new MeshWrapper(manager.assetsFileList, mesh, TexturedMaterialExtraProperties.Head);
                     meshList.Add(m);
                 }
             }

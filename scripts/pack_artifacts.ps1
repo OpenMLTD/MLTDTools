a3ed480b5a77c9038385a52530da48d9c9976ef2
George Wu <uiharu@buaa.edu.cn>
2020-06-05 19:11:29 +0200

    Add special name component to file that are in releases

--- scripts/pack_artifacts.ps1
+++ scripts/pack_artifacts.ps1
@@ -1,21 +1,38 @@
 param([String]$zipName = "miritore.zip")
 
+function Test-TagRelease()
+{
+    [String]$repoTag = $env:APPVEYOR_REPO_TAG
+
+    if ($null -eq $repoTag)
+    {
+        return $false
+    }
+    else
+    {
+        [Boolean]$r = $false
+        [Boolean]::TryParse($repoTag, [ref]$r)
+
+        return $r
+    }
+}
+
 [String]$configuration = $env:CONFIGURATION
 [String]$basePath = $env:APPVEYOR_BUILD_FOLDER
 
 $subPaths = @(
-    [String]::Format("src/AcbPack/bin/{0}", $configuration),
-    [String]::Format("src/HcaDec/bin/{0}", $configuration),
-    [String]::Format("src/ManifestExport/bin/{0}", $configuration),
-    [String]::Format("src/MillionDance/bin/x86/{0}", $configuration), # temporarily fixed to x86 because of AssetStudioUtilities
-    [String]::Format("src/MillionDanceView/bin/{0}", $configuration),
-    [String]::Format("src/MiriTore.Common/bin/{0}", $configuration),
-    [String]::Format("src/MiriTore.Logging/bin/{0}", $configuration),
-    [String]::Format("src/MltdInfoViewer/bin/{0}", $configuration),
-    #[String]::Format("src/ScenarioEdit/bin/{0}", $configuration),
-    [String]::Format("src/TDFacial/bin/{0}", $configuration), # remember to include facial_expr.json
-    [String]::Format("src/ManifestTools/bin/{0}", $configuration),
-    [String]::Empty
+[String]::Format("src/AcbPack/bin/{0}", $configuration),
+[String]::Format("src/HcaDec/bin/{0}", $configuration),
+[String]::Format("src/ManifestExport/bin/{0}", $configuration),
+[String]::Format("src/MillionDance/bin/x86/{0}", $configuration), # temporarily fixed to x86 because of AssetStudioUtilities
+[String]::Format("src/MillionDanceView/bin/{0}", $configuration),
+[String]::Format("src/MiriTore.Common/bin/{0}", $configuration),
+[String]::Format("src/MiriTore.Logging/bin/{0}", $configuration),
+[String]::Format("src/MltdInfoViewer/bin/{0}", $configuration),
+#[String]::Format("src/ScenarioEdit/bin/{0}", $configuration),
+[String]::Format("src/TDFacial/bin/{0}", $configuration), # remember to include facial_expr.json
+[String]::Format("src/ManifestTools/bin/{0}", $configuration),
+[String]::Empty
 );
 
 foreach ($subPath in $subPaths)
@@ -37,5 +54,17 @@ foreach ($subPath in $subPaths)
 
 & 7z a miritore.zip "${env:APPVEYOR_BUILD_FOLDER}\README.html"
 
-Push-AppveyorArtifact $zipName -FileName "miritore-appveyor-v${env:APPVEYOR_BUILD_VERSION}${env:RELEASE_SUFFIX}.zip" -DeploymentName "Versioned"
+[String]$isTagRelease = Test-TagRelease
+[String]$tagRelComp = if ($isTagRelease)
+{
+    "-rel"
+}
+else
+{
+    [String]::Empty
+}
+
+Push-AppveyorArtifact $zipName -FileName "miritore-appveyor${tagRelComp}-v${env:APPVEYOR_BUILD_VERSION}${env:RELEASE_SUFFIX}.zip" -DeploymentName "Versioned"
+
+# Latest artifact (half-permanent link)
 Push-AppveyorArtifact $zipName -FileName "miritore-appveyor-latest.zip" -DeploymentName "Latest"
